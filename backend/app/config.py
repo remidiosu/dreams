aae1ae6e7098fd7d70f654e9197f7afbb267b34a
gg <adanbekovag@gmail.com>
2026-02-07 17:51:48 +0500

    Revert "Fix Railway deployment: default db_connection, use $PORT"
    This reverts commit aa8c332d67493639913112a7fa783d5fb3bd9fe7.

--- backend/app/config.py
+++ backend/app/config.py
@@ -5,7 +5,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     secret_key: str
 
-    db_connection: str = ""
+    db_connection: str
     db_host: str
     db_port: int
     db_database: str
